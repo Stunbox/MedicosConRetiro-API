45df159af278a130d2e1a746a53721ad17cd9093
Stunbox <stunbox92@gmail.com>
2025-06-20 15:37:31 -0600

    Prject created

---
+++ api/index.ts
@@ -0,0 +1,65 @@
+require('dotenv').config()
+
+import { Resend } from 'resend';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import emailTemplate from '../templates/emailTemplate';
+
+const resend = new Resend('re_NPwxqQpj_29Srkb7P7on2wf2r1HqnvD4T');
+const app = express();
+const cors = require('cors');
+
+app.use(cors()); 
+app.use(bodyParser.json());
+
+let corsOptions = {
+  origin: [ 'http://localhost:4321/']
+};
+
+//index
+app.get('/',async (req:Request,res:Response) => {
+  res.status(200).json({message: "Bienvenido !"});
+});
+//test
+app.get('/test',async (req:Request,res:Response) => {
+  res.json({requestBody: req.body});
+});
+//Send email
+app.post('/send',cors(corsOptions), async (req: Request, res: Response) => {
+
+  var template = emailTemplate
+    .replace("{userName}",req.body.name)
+    .replace("{phone}",req.body.phone)
+    .replace("{email}",req.body.email)
+    .replace("{email2}",req.body.email)
+    .replace("{type_message}",req.body.typeMessage)
+    .replace("{message}",req.body.message);
+  try {
+    const data = await resend.emails.send({
+      from: 'noreply@medicosconretiro.com.mx',
+      to: ['ventas@sinergiaeneventos.com','eduardo@medicosconretiro.com.mx'],
+      subject: req.body.name +" solicito informacion",
+      html: template,
+    });
+    const dataClient = await resend.emails.send({
+      from: 'noreply@medicosconretiro.com.mx',
+      to: [req.body.email],
+      subject: "Gracias por comunicarte con el club",
+      html: template,
+    });
+
+    res.status(200).json({"status":200,"data":{"idMessageToAgent": data, "idMessageToUser":dataClient}});
+  } catch(error) {
+    res.status(400).json({"status":400,"data":error});
+  }
+});
+
+app.listen(3000, () => {
+  /*if (!process.env.RESEND_API_KEY) {
+    throw `Abort: You need to define RESEND_API_KEY in the .env file.`;
+  }*/
+  
+  console.log('Listening on http://localhost:3000');
+});
+
+module.exports = app;
